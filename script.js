bb7beb887b018a4dbdd8432ad3f85b7ed085a655
TheDanishMexican <danieljchristjansen@gmail.com>
2023-03-09 15:36:22 +0100

    popk

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 "use strict";
 
-let admin;
-let firstName = "John";
+const n1 = 1;
+const n2 = 2;
 
-admin = firstName;
+const s1 = "1";
+const s2 = "2";
 
-console.log(admin);
+console.log(n1 + n2);
+console.log(s1 + s2);
+
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log(true + false);
+console.log(6 / "3");
+console.log(" 2 " * " 3 ");
+console.log(4 + 5 + "px");
